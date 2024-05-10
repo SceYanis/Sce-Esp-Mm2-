901c87ce71c79faad4bc80b0b7fe59cb61ec3bd2
SceYanis <168266241+SceYanis@users.noreply.github.com>
2024-05-10 19:59:20 +0200

    main.lua

--- main.lua
+++ main.lua
@@ -1,65 +1,176 @@
-local function murderer()
-    for i, v in pairs(game.Players:GetPlayers()) do
-        if v.Backpack:FindFirstChild("Knife") or v.Character:FindFirstChild("Knife") then
-            return v
-        end
-    end
-end
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local Camera = workspace.CurrentCamera
 
-local function sherrif()
-    for i, v in pairs(game.Players:GetPlayers()) do
-        if v.Backpack:FindFirstChild("Gun") or v.Character:FindFirstChild("Gun") then
-            return v
-        end
-    end
-end
+local Typing = false
 
-game:GetService("UserInputService").InputBegan:Connect(function(inputObject, gameProcessedEvent)
-    if inputObject.KeyCode == Enum.KeyCode.E then
-        for i, v in pairs(game.Players:GetPlayers()) do
-            if v.Backpack:FindFirstChild("Knife") or v.Character:FindFirstChild("Knife") then
-                local esp = Instance.new("BoxHandleAdornment")
-                esp.Parent = v.Character.Head
-                esp.Size = Vector3.new(1,1,1)
-                esp.Color3 = Color3.new(255, 0, 0)
-                esp.AlwaysOnTop = true
-                esp.Adornee = v.Character.Head
-                esp.Visible = true
-                esp.ZIndex = 2
-            elseif v.Backpack:FindFirstChild("Gun") or v.Character:FindFirstChild("Gun") then
-                local esp = Instance.new("BoxHandleAdornment")
-                esp.Parent = v.Character.Head
-                esp.Size = Vector3.new(1,1,1)
-                esp.Color3 = Color3.new(0, 0, 255)
-                esp.AlwaysOnTop = true
-                esp.Adornee = v.Character.Head
-                esp.Visible = true
-                esp.ZIndex = 2
-            else
-                local esp = Instance.new("BoxHandleAdornment")
-                esp.Parent = v.Character.Head
-                esp.Size = Vector3.new(1,1,1)
-                esp.Color3 = Color3.new(0, 255, 0)
-                esp.AlwaysOnTop = true
-                esp.Adornee = v.Character.Head
-                esp.Visible = true
-                esp.ZIndex = 2
-            end
-        end
-    end
-end)
+_G.SendNotifications = true   
+_G.DefaultSettings = false   
 
-game:GetService("UserInputService").InputBegan:Connect(function(inputObject, gameProcessedEvent)
-    if inputObject.KeyCode == Enum.KeyCode.F then
-        for i, v in pairs(game.Players:GetPlayers()) do
-            for i, a in pairs(v.Character.Head:GetChildren()) do
-                if a:IsA("BoxHandleAdornment") then
-                    a:Remove()
+_G.TeamCheck = false   
+_G.ESPVisible = true   
+_G.TextColor = Color3.fromRGB(255, 80, 10)   
+_G.TextSize = 14   
+_G.Center = true   
+_G.Outline = true   
+_G.OutlineColor = Color3.fromRGB(0, 0, 0)   
+_G.TextTransparency = 0.7   
+_G.TextFont = Drawing.Fonts.UI   
+
+_G.DisableKey = Enum.KeyCode.Q   
+
+local function CreateESP()
+    for _, v in next, Players:GetPlayers() do
+        if v.Name ~= Players.LocalPlayer.Name then
+            local ESP = Drawing.new("Text")
+
+            RunService.RenderStepped:Connect(function()
+                if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                    local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+
+                    ESP.Size = _G.TextSize
+                    ESP.Center = _G.Center
+                    ESP.Outline = _G.Outline
+                    ESP.OutlineColor = _G.OutlineColor
+                    ESP.Color = _G.TextColor
+                    ESP.Transparency = _G.TextTransparency
+                    ESP.Font = _G.TextFont
+
+                    if OnScreen == true then
+                        local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                        local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                        local Dist = (Part1 - Part2).Magnitude
+                        ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                        ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                        if _G.TeamCheck == true then 
+                            if Players.LocalPlayer.Team ~= v.Team then
+                                ESP.Visible = _G.ESPVisible
+                            else
+                                ESP.Visible = false
+                            end
+                        else
+                            ESP.Visible = _G.ESPVisible
+                        end
+                    else
+                        ESP.Visible = false
+                    end
+                else
+                    ESP.Visible = false
                 end
+            end)
+
+            Players.PlayerRemoving:Connect(function()
+                ESP.Visible = false
+            end)
+        end
+    end
+
+    Players.PlayerAdded:Connect(function(Player)
+        Player.CharacterAdded:Connect(function(v)
+            if v.Name ~= Players.LocalPlayer.Name then 
+                local ESP = Drawing.new("Text")
+    
+                RunService.RenderStepped:Connect(function()
+                    if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                        local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+    
+                        ESP.Size = _G.TextSize
+                        ESP.Center = _G.Center
+                        ESP.Outline = _G.Outline
+                        ESP.OutlineColor = _G.OutlineColor
+                        ESP.Color = _G.TextColor
+                        ESP.Transparency = _G.TextTransparency
+    
+                        if OnScreen == true then
+                            local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                        local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                            local Dist = (Part1 - Part2).Magnitude
+                            ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                            ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                            if _G.TeamCheck == true then 
+                                if Players.LocalPlayer.Team ~= Player.Team then
+                                    ESP.Visible = _G.ESPVisible
+                                else
+                                    ESP.Visible = false
+                                end
+                            else
+                                ESP.Visible = _G.ESPVisible
+                            end
+                        else
+                            ESP.Visible = false
+                        end
+                    else
+                        ESP.Visible = false
+                    end
+                end)
+    
+                Players.PlayerRemoving:Connect(function()
+                    ESP.Visible = false
+                end)
             end
+        end)
+    end)
+end
+
+if _G.DefaultSettings == true then
+    _G.TeamCheck = false
+    _G.ESPVisible = true
+    _G.TextColor = Color3.fromRGB(40, 90, 255)
+    _G.TextSize = 14
+    _G.Center = true
+    _G.Outline = false
+    _G.OutlineColor = Color3.fromRGB(0, 0, 0)
+    _G.DisableKey = Enum.KeyCode.Q
+    _G.TextTransparency = 0.75
+end
+
+UserInputService.TextBoxFocused:Connect(function()
+    Typing = true
+end)
+
+UserInputService.TextBoxFocusReleased:Connect(function()
+    Typing = false
+end)
+
+UserInputService.InputBegan:Connect(function(Input)
+    if Input.KeyCode == _G.DisableKey and Typing == false then
+        _G.ESPVisible = not _G.ESPVisible
+        
+        if _G.SendNotifications == true then
+            game:GetService("StarterGui"):SetCore("SendNotification",{
+                Title = "Exunys Developer";
+                Text = "The ESP's visibility is now set to "..tostring(_G.ESPVisible)..".";
+                Duration = 5;
+            })
         end
     end
 end)
+
+local Success, Errored = pcall(function()
+    CreateESP()
+end)
+
+if Success and not Errored then
+    if _G.SendNotifications == true then
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Exunys Developer";
+            Text = "ESP script has successfully loaded.";
+            Duration = 5;
+        })
+    end
+elseif Errored and not Success then
+    if _G.SendNotifications == true then
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Exunys Developer";
+            Text = "ESP script has errored while loading, please check the developer console! (F9)";
+            Duration = 5;
+        })
+    end
+    TestService:Message("The ESP script has errored, please notify Exunys with the following information :")
+    warn(Errored)
+    print("!! IF THE ERROR IS A FALSE POSITIVE (says that a player cannot be found) THEN DO NOT BOTHER !!")
+end
 
 -- discord.gg/924ycm7jr6
 -- by slayer946
